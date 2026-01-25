2100787603815f7343520431608768e45548d074
Jandi3258 <pawel.jandula@vp.pl>
2026-01-25 10:47:01 +0100

    Poprawione UI oraz dodanie AnimalType do latwiejszego obslugiwania

--- BlazorApp1/Models/Player.cs
+++ BlazorApp1/Models/Player.cs
@@ -1,3 +1,5 @@
+namespace BlazorApp1.Models;
+
 public class Player
 {
     public int Rabbits { get; set; }
@@ -7,4 +9,7 @@ public class Player
     public int Horses { get; set; }
     public bool HasSmallDog { get; set; }
     public bool HasBigDog { get; set; }
+
+    public bool HasWon() => 
+        Rabbits > 0 && Sheep > 0 && Pigs > 0 && Cows > 0 && Horses > 0;
 }
