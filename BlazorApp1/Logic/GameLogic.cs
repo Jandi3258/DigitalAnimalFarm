752562deb32f2e90c88b5d66e9e4e10dfacba480
podhay123 <kuba.pod123@gmail.com>
2026-01-27 18:19:29 +0100

    delete: unused file again

--- BlazorApp1/Logic/GameLogic.cs
+++
@@ -1,8 +0,0 @@
-public static class GameLogic
-{
-    public static int CalculateNewAnimals(int currentCount, int rolledCount)
-    {
-        if (rolledCount == 0) return 0;
-        return (currentCount + rolledCount) / 2;
-    }
-}
